076868c753a52835a0df506a86499238653bb71f
dzdn <jdustinwilliams@gmail.com>
2023-09-17 17:28:35 -0400

    Rearrange URLs, making products a subdir

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", include("products.urls"), name="products"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("products/", include("products.urls"), name="products"),
     path("admin/", admin.site.urls),
 ]
 

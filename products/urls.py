fe5853005633fbe5ab929708d5896e1a30c8e23f
dzdn <jdustinwilliams@gmail.com>
2023-09-17 13:49:13 -0400

    Add ListView & template for Product

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views import ProductListView
+
 # from .views import ...
 
 urlpatterns = [
-    #    path("", ProductListView.as_view(), name="products"),
+    path("", ProductListView.as_view(), name="products"),
 ]
